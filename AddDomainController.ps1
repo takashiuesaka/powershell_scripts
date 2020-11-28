45a1260180aea8530272f1ec6a86df27ee680a25
takashiuesaka <61622933+takashiuesaka@users.noreply.github.com>
2020-11-28 15:27:06 +0900

    Install New Forest

--- AddDomainController.ps1
+++ AddDomainController.ps1
@@ -1,3 +1,11 @@
+Param(
+    [parameter(mandatory=$true)][String]$plainTextPassword,
+    [parameter(mandatory=$true)][String]$domain
+)
+
+# Create Secure Password
+$password = ConvertTo-SecureString -String $plainTextPassword -AsPlainText -Force
+
 function Disable-InternetExplorerESC {
     $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
     $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
@@ -18,4 +26,5 @@ Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 # Set-WinSystemLocale -SystemLocale ja-JP
 Set-TimeZone -Id "Tokyo Standard Time"
 
-# Restart-Computer
+# Install New Forest
+Install-ADDSForest -DomainName $domain -InstallDNS --SafeModeAdministratorPassword $password -Force
